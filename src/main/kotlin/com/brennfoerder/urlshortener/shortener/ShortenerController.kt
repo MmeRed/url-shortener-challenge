29e5a1dcde0c79532bd94633309404a373eac5e5
Janine Brennfoerder <janine.brennfoerder@gmail.com>
2022-08-04 17:44:54 +0200

    add shorten endpoint

--- src/main/kotlin/com/brennfoerder/urlshortener/shortener/ShortenerController.kt
+++ src/main/kotlin/com/brennfoerder/urlshortener/shortener/ShortenerController.kt
@@ -2,6 +2,8 @@ package com.brennfoerder.urlshortener.shortener
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,8 +13,21 @@ class ShortenerController {
     fun decodeUrl(@PathVariable toDecode: String): DecodedUrlDto {
         return DecodedUrlDto("https://www.dkbcodefactory.com")
     }
+
+    @PostMapping("/shorten")
+    fun shortenUrl(@RequestBody urlToShortenDto: UrlToShortenDto): ShortenedUrlDto {
+        return ShortenedUrlDto("codefactory")
+    }
 }
 
 data class DecodedUrlDto(
     val decodedUrl: String
+)
+
+data class UrlToShortenDto(
+    val url: String
+)
+
+data class ShortenedUrlDto(
+    val shortenedUrl: String
 )
--- src/test/kotlin/com/brennfoerder/urlshortener/shortener/ShortenerControllerTest.kt
+++ src/test/kotlin/com/brennfoerder/urlshortener/shortener/ShortenerControllerTest.kt
@@ -1,11 +1,14 @@
 package com.brennfoerder.urlshortener.shortener
 
 import org.hamcrest.CoreMatchers.equalTo
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 
 @WebMvcTest
 internal class ShortenerControllerTest(
@@ -13,11 +16,30 @@ internal class ShortenerControllerTest(
 ) {
 
     @Test
-    fun decodeUrl() {
+    fun `decode url - success`() {
         mockMvc.get("/codefactory")
             .andExpect {
                 status { isOk() }
                 jsonPath("decodedUrl", equalTo("https://www.dkbcodefactory.com"))
             }
+    }
+
+    @Test
+    fun `encode url - success`() {
+        @Language("JSON")
+        val request = """
+            {
+              "url": "https://www.dkbcodefactory.com"
+            }
+        """.trimIndent()
+
+        mockMvc.post("/shorten") {
+            contentType = MediaType.APPLICATION_JSON
+            content = request
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+            jsonPath("shortenedUrl", equalTo("codefactory"))
+        }
     }
 }
